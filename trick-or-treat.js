9db233384ab11a0860d68f2f4796d9a4e1e0db60
QPDoyle <qpdoyle13@gmail.com>
2024-10-25 08:50:45 -0400

    added 2 functions

---
+++ trick-or-treat.js
@@ -0,0 +1,8 @@
+function CandyBag() {
+    const array = ["Twix", "Skittles", "Snickers", "Sour Patch Kids", "Kit-Kat"]
+}
+function pickCandy(bag) {
+    return bag[Math.floor(Math.random() * 5)];
+}
+
+
