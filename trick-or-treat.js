caee6c9f9394041993830914aacdfb3883597d85
QPDoyle <qpdoyle13@gmail.com>
2024-10-25 08:54:04 -0400

    3 functions

--- trick-or-treat.js
+++ trick-or-treat.js
@@ -1,6 +1,11 @@
-function CandyBag() {
-    const array = ["Twix", "Skittles", "Snickers", "Sour Patch Kids", "Kit-Kat"]
+function CandyBag(c1, c2, c3, c4, c5) {
+    const bagArray = [c1, c2, c3, c4, c5];
 }
+
+const createBag=()=> {
+    const candyBag1 = new CandyBag("Twix", "Skittles", "Snickers", "Sour Patch Kids", "Kit-Kat");
+}
+
 function pickCandy(bag) {
     return bag[Math.floor(Math.random() * 5)];
 }
